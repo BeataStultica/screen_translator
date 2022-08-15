890833a347cd1fb8d4f539db2c6feb01fe5346c6
Dmytro <dimon2000.1982@gmail.com>
2022-08-15 21:52:01 +0300

    add deepl translate, finish test version

--- test.py
+++ test.py
@@ -1,35 +1,48 @@
 import threading
 from paddleocr import PaddleOCR
 import time
-from PIL import Image, ImageTk, ImageDraw, ImageGrab
+from PIL import Image, ImageTk, ImageDraw, ImageGrab, ImageFont
 import tkinter, win32api, win32con, pywintypes
-import imagesize
 import pyWinhook
 import pythoncom
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pyperclip
+
 ocr = PaddleOCR(use_angle_cls=True, lang='en')
 a = 0
 next_img = 0
 coordinate = [1, 1, 1, 1]
-def output():
-    global next_img 
-    #t = time.time()
-    #res = ocr.ocr('./example/images/test2.png', cls=True)
-    #print(res)
-    #print(time.time()-t)
 
+chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument('user-data-dir=C:\\Users\\dimon\\AppData\\Local\\Google\\Chrome\\User Data')
+browser = webdriver.Chrome(chrome_options=chrome_options) 
+browser.get('https://www.deepl.com/translator#en/ru/')
+def deeptr(text):
+    br = browser.find_element('xpath','//textarea[@dl-test="translator-source-input"]')
+    br.clear()
+    pyperclip.copy(text)
+    #time.sleep(5)
+    br.send_keys(Keys.CONTROL + "v")
+    c = text.count('999909')
+    count = 0
+    while True:
+
+        time.sleep(0.1)
+        b = browser.find_element('id', 'target-dummydiv')
+        res = b.get_attribute('innerHTML')
+        if res.count('999909') >= c:
+            pyperclip.copy(text)
+            return res.split('999909')#replace('999909', '')
+        print(res.count('999909'))
+        print(c)
+def output(scale=2, fontsize = 15):
+    global next_img 
 
     root = tkinter.Tk()
-
-    #width, height = imagesize.get('./example/images/test2.png')
     img = Image.new('RGBA', (100, 100), (255, 0, 0, 0))
-
-    #draw = ImageDraw.Draw(img)
-    #for i in res:
-    #    print(i[0][0])
-    #    draw.text((i[0][0][0], i[0][0][1]), i[1][0], fill=(255, 0, 0))
-
     test = ImageTk.PhotoImage(img)
-    label = tkinter.Label(image=test, bg='white')#text='Text on the screen', font=('Times New Roman','80'), fg='black', bg='white')
+    label = tkinter.Label(image=test, bg='white')
     label.master.overrideredirect(True)
     label.master.geometry("+250+250")
     label.master.lift()
@@ -47,10 +60,7 @@ def output():
     print('-')
 
     while True:
-        #label.update()
-        #time.sleep(1)
-        
-        if next_img:# and (coordinate[0]-coordinate[2]) > 4 and (coordinate[1]-coordinate[3]) > 4:
+        if next_img:
             print('--')
             label.configure(image=test)
             label.update()
@@ -58,32 +68,32 @@ def output():
             width, height = coordinate[0], coordinate[1]
             label.master.geometry("+"+str(width)+"+" +str(height))
             pic1 = ImageGrab.grab(coordinate)
-            pic = pic1.resize((pic1.width//2, pic1.height//2))
+            pic = pic1.resize((pic1.width//scale, pic1.height//scale))
             pic.save('temp.png', 'PNG')
             res = ocr.ocr('temp.png', cls=True)
             print(res)
             img3 = Image.new('RGBA', (pic1.width, pic1.height), (255, 0, 0, 0))
             draw = ImageDraw.Draw(img3)
-            for i in res:
-            #    print(i[0][0])
-                draw.rectangle((i[0][0][0]*2, i[0][0][1]*2, i[0][2][0]*2, i[0][2][1]*2), fill="grey")
-                draw.text((i[0][0][0]*2, i[0][0][1]*2), i[1][0], fill=(255, 0, 0))
-            #draw.text((10,10), str(coordinate), fill=(255, 0, 0))
+            font = ImageFont.truetype('arial.ttf', fontsize)
+            tran_str = ''
+            for r in res:
+                tran_str +=r[1][0] + ' 999909 '
+            tr_res = deeptr(tran_str)
+            while len(tr_res) < len(res):
+                tr_res.append('')
+            for c, i in enumerate(res):
+                draw.rectangle((i[0][0][0]*scale, i[0][0][1]*scale, i[0][2][0]*scale, i[0][2][1]*scale), fill="grey")
+                draw.text((i[0][0][0]*scale, i[0][0][1]*scale), tr_res[c], fill=(0, 0, 0), font = font)
             img3 =  ImageTk.PhotoImage(img3)
             label.configure(image=img3)
             next_img=0
-        #label.image = img3 
-        #label.im = img.rotate(10)
-        #label.master.geometry("+"+str(r) +"+"+str(r))
-        #r +=5
-        #print(r)
         label.update()
 def change(e):
     global a
     global next_img
     if e.Key == 'Lcontrol':
         a =1
-    elif e.Key == 'Z':
+    elif e.Key == 'P':
         next_img =1
     else:
         print(e.Key)
@@ -103,10 +113,8 @@ def select_data():
     hm.KeyDown = change
     hm.HookMouse()
     hm.HookKeyboard()
-    #keyboard.hook(change)
     while True:
         pythoncom.PumpMessages()
-        #keyboard.wait()
 if __name__ == '__main__':
     thread1 = threading.Thread(target=select_data)
     thread2 = threading.Thread(target=output)
